dd62ba4129c72cb317e20e3f308c1a0efe14ec38
wengqinghui <3113744638@qq.com>
2024-09-09 13:26:51 +0800

    用户下单功能代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单
+ *
+ * 为什么还要重新指定一下这个bean的名字？？？
+ * 因为后面再admin里面也需要创建一个OrderController，即在商家这端也需要
+ *    对订单进行操作，所以为了防止它们重名了指定一个bean的名字。
+ */
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Slf4j
+@Api(tags = "C端-订单接口")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     *
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    @ApiOperation("用户下单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用户下单：{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+
+}
+
