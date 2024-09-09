2fa91ad227bffe6e72d9b744cbb82b802bc8183f
wengqinghui <3113744638@qq.com>
2024-09-10 00:34:25 +0800

    订单数据统计代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -71,5 +71,11 @@ public interface OrderMapper {
      */
     Double sumByMap(Map map);
 
+    /**
+     *根据动态条件统计订单数量
+     * @param map
+     */
+    Integer countByMap(Map map);
+
 }
 
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -4,6 +4,7 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -152,6 +153,115 @@ public class ReportServiceImpl implements ReportService {
         map.put("end", endTime);
         return userMapper.countByMap(map);
     }
+
+    /**
+     * 根据时间区间统计订单数量
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end){
+        //1.准备日期条件：和营业额功能相同，不在赘述。
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //同样需要把集合类型转化为字符串类型并用逗号分隔
+        String data = StringUtils.join(dateList, ",");
+
+        //2.准备每一天对应的订单数量：订单总数  有效订单数
+        //每天订单总数集合
+        List<Integer> orderCountList = new ArrayList<>();
+        //每天有效订单数集合
+        List<Integer> validOrderCountList = new ArrayList<>();
+
+        /**
+         * 思路分析：查询的是订单表
+         * 查询每天的总订单数：只需要根据下单时间计算出当天的起始时间和结束时间作为查询条件，
+         *                 就是当天的总订单数。
+         * 查询每天的有效订单数：根据下单时间计算出当天的起始时间和结束时间以及状态已完成（代表有效订单）的订单作为查询条件，
+         *                   就是每天的有效订单数
+         * 同样没必要写2个sql，因为这2个SQL的主体结构相同，只是查询的条件不同，所以没有必要写2个sql只需要写一个动态的sql即可。
+         */
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);//起始时间 包含年月日时分秒
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);//结束时间
+            //查询每天的总订单数 select count(id) from orders where order_time > ? and order_time < ?
+            Integer orderCount = getOrderCount(beginTime, endTime, null);
+
+            //查询每天的有效订单数 select count(id) from orders where order_time > ? and order_time < ? and status = ?
+            Integer validOrderCount = getOrderCount(beginTime, endTime, Orders.COMPLETED);
+
+            orderCountList.add(orderCount);
+            validOrderCountList.add(validOrderCount);
+        }
+
+        //同样需要把集合类型转化为字符串类型并用逗号分隔
+        String orderCount1 = StringUtils.join(orderCountList, ",");//每天订单总数集合
+        String validOrderCount1 = StringUtils.join(validOrderCountList, ",");//每天有效订单数集合
+
+        /**
+         * 3. 准备时间区间内的订单数：时间区间内的总订单数   时间区间内的总有效订单数
+         * 思路分析：
+         *    订单总数：整个这个时间区域内订单总的数量，根据这个时间去卡查询数据库可以查询出来。
+         *    时间区间内的总有效订单数：也可以通过查询数据库查出来。
+         *    实际上不查询数据库，总订单数和总有效订单数也能计算出来：
+         *         因为上面2个集合中已经查询保存了，这个时间段之内每天的总订单数和总有效订单数，
+         *         所以只需要分别遍历这2个集合，每天的订单总数加一起就是整个时间段总订单数，
+         *         每天的有效订单数加起来就是整个时间段的总有效订单数。
+         */
+        //计算时间区域内的总订单数量
+        //Integer totalOrderCounts = orderCountList.stream().reduce(Integer::sum).get();//方式一：简写方式
+        Integer totalOrderCounts = 0;
+        for (Integer integer : orderCountList) {  //方式二：普通for循环方式
+            totalOrderCounts = totalOrderCounts+integer;
+        }
+        //计算时间区域内的总有效订单数量
+        //Integer validOrderCounts = validOrderCountList.stream().reduce(Integer::sum).get();//方式一：简写方式
+        Integer validOrderCounts = 0;
+        for (Integer integer : validOrderCountList) { //方式二：普通for循环方式
+            validOrderCounts = validOrderCounts+integer;
+        }
+
+        //4.订单完成率：  总有效订单数量/总订单数量=订单完成率
+        Double orderCompletionRate = 0.0;  //订单完成率的初始值
+        if(totalOrderCounts != 0){ //防止分母为0出现异常
+            //总有效订单数量和总有效订单数量都是Integer类型，这里使用的是Double类型接收所以需要进行转化
+            orderCompletionRate = validOrderCounts.doubleValue() / totalOrderCounts;
+        }
+
+        //构造vo对象
+        return OrderReportVO.builder()
+                .dateList(data)  //x轴日期数据
+                .orderCountList(orderCount1) //y轴每天订单总数
+                .validOrderCountList(validOrderCount1)//y轴每天有效订单总数
+                .totalOrderCount(totalOrderCounts) //时间区域内总订单数
+                .validOrderCount(validOrderCounts) //时间区域内总有效订单数
+                .orderCompletionRate(orderCompletionRate) //订单完成率
+                .build();
+
+    }
+
+    /**
+     * 根据时间区间统计指定状态的订单数量
+     * @param beginTime
+     * @param endTime
+     * @param status
+     * @return
+     */
+    private Integer getOrderCount(LocalDateTime beginTime, LocalDateTime endTime, Integer status) {
+        //封装sql查询的条件为map集合，因为设计的mapper层传递的参数是使用map来封装的
+        Map map = new HashMap();
+        map.put("status", status);
+        map.put("begin",beginTime);
+        map.put("end", endTime);
+        return orderMapper.countByMap(map);
+    }
+
 
 }
 
